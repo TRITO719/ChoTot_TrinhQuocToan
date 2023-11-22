80bd7aba5edd5aee8a7b80eb59ee87a08fcf227d
Trito <trito719@gmail.com>
2023-11-22 14:46:58 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen1 from './Screen/Screen1'
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Screen1}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Screen/Screen1.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import {StyleSheet,Text,View,TextInput,Pressable,ScrollView,Image,} from "react-native";
+
+export default function Screen1(){
+    return{
+        
+    };
+}
