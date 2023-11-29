3ca677678d690fd90982eb6c70d37fba753a33de
TRITO719 <95730847+TRITO719@users.noreply.github.com>
2023-11-29 07:52:27 +0700

    Delete Screen directory

--- Screen/Screen1.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from "react";
-import {StyleSheet,Text,View,TextInput,Pressable,ScrollView,Image,} from "react-native";
-
-export default function Screen1(){
-    return{
-        
-    };
-}
